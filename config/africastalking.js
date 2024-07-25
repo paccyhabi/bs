3c0e8852c0dfad85c3d9a846680f5e2138d16789
paccyhabi <paccyhabi@gmail.com>
2024-07-25 10:08:02 +0300

    api key

--- config/africastalking.js
+++ config/africastalking.js
@@ -1,6 +1,6 @@
 const africastalking = require('africastalking')({
-    apiKey: 'your_api_key', // Replace with your Africa's Talking API key
-    username: 'your_username' // Replace with your Africa's Talking username
+    apiKey: 'atsk_e2b70129cfeaa091828f53875d67bfb170a48ff8a4069b717ebc953eb06777deb221b430', // Replace with your Africa's Talking API key
+    username: 'sandbox' // Replace with your Africa's Talking username
 });
 
 module.exports = africastalking;
