8b79af39a096c52507d6eb788e0a598be8b34354
paccyhabi <140205164+paccyhabi@users.noreply.github.com>
2024-10-01 12:03:00 +0200

    Update dbconfig.js

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,7 +1,7 @@
 module.exports = {
-    HOST: 'bda8vgi3njixgdouoorf-mysql.services.clever-cloud.com',
-    USER: 'umzktoadrd5kfyml',
-    PASSWORD: 'x7bnDB30c6gJ6OXi7G09',
-    DB:'bda8vgi3njixgdouoorf',
+    HOST: 'bust7tcar4t2dsotbnt3-mysql.services.clever-cloud.com',
+    USER: 'ujp26f3ewgomzjur',
+    PASSWORD: '2hmrqxpnyxkRHUDLDBUW',
+    DB:'bust7tcar4t2dsotbnt3',
     dialect: 'mysql'
 }
